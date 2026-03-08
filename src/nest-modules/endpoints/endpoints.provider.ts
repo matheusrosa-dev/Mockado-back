10b2bce8e122a91a539a5bd8e0788ebe0bf4ab01
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-03-08 20:04:19 -0300

    refactor: streamline use case provider definitions for improved maintainability

--- src/nest-modules/endpoints/endpoints.provider.ts
+++ src/nest-modules/endpoints/endpoints.provider.ts
@@ -18,10 +18,12 @@ const REPOSITORIES = {
     provide: "EndpointRepository",
     useExisting: EndpointTypeOrmRepository,
   } as ExistingProvider,
+
   ENDPOINT_IN_MEMORY_REPOSITORY: {
     provide: EndpointInMemoryRepository,
     useClass: EndpointInMemoryRepository,
   } as ClassProvider,
+
   ENDPOINT_TYPEORM_REPOSITORY: {
     provide: EndpointTypeOrmRepository,
     useFactory: (dataSource: DataSource) =>
@@ -30,40 +32,22 @@ const REPOSITORIES = {
   } as FactoryProvider,
 };
 
-const USE_CASES: Record<string, FactoryProvider> = {
-  LIST_ENDPOINTS_USE_CASE: {
-    provide: ListEndpointsUseCase,
-    useFactory: (endpointRepository: IEndpointRepository) =>
-      new ListEndpointsUseCase(endpointRepository),
-    inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
-  },
-  CREATE_ENDPOINT_USE_CASE: {
-    provide: CreateEndpointUseCase,
-    useFactory: (endpointRepository: IEndpointRepository) =>
-      new CreateEndpointUseCase(endpointRepository),
-    inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
-  },
-  UPDATE_ENDPOINT_USE_CASE: {
-    provide: UpdateEndpointUseCase,
-    useFactory: (endpointRepository: IEndpointRepository) =>
-      new UpdateEndpointUseCase(endpointRepository),
-    inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
-  },
-  FIND_ENDPOINT_USE_CASE: {
-    provide: FindEndpointUseCase,
-    useFactory: (endpointRepository: IEndpointRepository) =>
-      new FindEndpointUseCase(endpointRepository),
-    inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
-  },
-  DELETE_ENDPOINT_USE_CASE: {
-    provide: DeleteEndpointUseCase,
-    useFactory: (endpointRepository: IEndpointRepository) =>
-      new DeleteEndpointUseCase(endpointRepository),
-    inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
-  },
-};
+const useCases = [
+  ListEndpointsUseCase,
+  CreateEndpointUseCase,
+  UpdateEndpointUseCase,
+  FindEndpointUseCase,
+  DeleteEndpointUseCase,
+];
+
+const USE_CASES_PROVIDERS: FactoryProvider[] = useCases.map((useCase) => ({
+  provide: useCase,
+  useFactory: (endpointRepository: IEndpointRepository) =>
+    new useCase(endpointRepository),
+  inject: [REPOSITORIES.ENDPOINT_REPOSITORY.provide],
+}));
 
 export const ENDPOINT_PROVIDERS = {
   REPOSITORIES,
-  USE_CASES,
+  USE_CASES: USE_CASES_PROVIDERS,
 };
