a94d411e304eb419e62578259accd722e985b78d
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-03-08 12:58:01 -0300

    refactor: simplify method implementations and improve code readability in EndpointFakeBuilder

--- src/core/domain/endpoint/endpoint-fake.builder.ts
+++ src/core/domain/endpoint/endpoint-fake.builder.ts
@@ -6,40 +6,26 @@ import { HttpMethod, ResponseBodyType } from "./endpoint.types";
 type PropOrFactory<T> = T | (() => T);
 
 export class EndpointFakeBuilder<TBuild = Endpoint | Endpoint[]> {
+  private readonly chance = Chance();
+
   private _endpoint_id?: PropOrFactory<Uuid>;
   private _method: PropOrFactory<HttpMethod> = () =>
-    this.chance.pickone([
-      HttpMethod.GET,
-      HttpMethod.POST,
-      HttpMethod.PUT,
-      HttpMethod.DELETE,
-      HttpMethod.PATCH,
-    ]) as HttpMethod;
+    this.chance.pickone(Object.values(HttpMethod));
   private _title: PropOrFactory<string> = () => this.chance.word();
   private _description: PropOrFactory<string | undefined> = () =>
-    this.chance.paragraph({
-      sentences: 1,
-    });
+    this.chance.paragraph({ sentences: 1 });
   private _delay: PropOrFactory<number | undefined> = () =>
     this.chance.integer({ min: 0, max: 10 });
   private _statusCode: PropOrFactory<number> = () =>
-    this.chance.pickone([
-      200, 201, 204, 400, 401, 403, 404, 500, 502, 503,
-    ]) as number;
+    this.chance.pickone([200, 201, 204, 400, 401, 403, 404, 500, 502, 503]);
   private _responseBodyType: PropOrFactory<ResponseBodyType> = () =>
-    this.chance.pickone([
-      ResponseBodyType.JSON,
-      ResponseBodyType.TEXT,
-      ResponseBodyType.NULL,
-      ResponseBodyType.EMPTY,
-    ]) as ResponseBodyType;
+    this.chance.pickone(Object.values(ResponseBodyType));
   private _responseJson: PropOrFactory<string> = () =>
     JSON.stringify({ message: this.chance.sentence() });
   private _responseText: PropOrFactory<string> = () => this.chance.sentence();
   private _createdAt?: PropOrFactory<Date>;
 
-  private amount: number;
-  private chance: Chance.Chance;
+  private constructor(private readonly amount = 1) {}
 
   static oneEndpoint() {
     return new EndpointFakeBuilder<Endpoint>();
@@ -49,23 +35,18 @@ export class EndpointFakeBuilder<TBuild = Endpoint | Endpoint[]> {
     return new EndpointFakeBuilder<Endpoint[]>(amount);
   }
 
-  private constructor(amount = 1) {
-    this.amount = amount;
-    this.chance = Chance();
-  }
-
   withEndpointId(valueOrFactory: PropOrFactory<Uuid>) {
     this._endpoint_id = valueOrFactory;
     return this;
   }
 
-  withTitle(valueOrFactory: PropOrFactory<string>) {
-    this._title = valueOrFactory;
+  withMethod(valueOrFactory: PropOrFactory<HttpMethod>) {
+    this._method = valueOrFactory;
     return this;
   }
 
-  withMethod(valueOrFactory: PropOrFactory<HttpMethod>) {
-    this._method = valueOrFactory;
+  withTitle(valueOrFactory: PropOrFactory<string>) {
+    this._title = valueOrFactory;
     return this;
   }
 
@@ -105,7 +86,7 @@ export class EndpointFakeBuilder<TBuild = Endpoint | Endpoint[]> {
   }
 
   build(): TBuild {
-    const endpoints = new Array(this.amount).fill(undefined).map(() => {
+    const endpoints = Array.from({ length: this.amount }, () => {
       const endpoint = new Endpoint({
         ...(this._endpoint_id && {
           endpoint_id: this.callFactory(this._endpoint_id),
@@ -113,12 +94,12 @@ export class EndpointFakeBuilder<TBuild = Endpoint | Endpoint[]> {
 
         method: this.callFactory(this._method),
         title: this.callFactory(this._title),
+        description: this.callFactory(this._description),
+        delay: this.callFactory(this._delay),
         statusCode: this.callFactory(this._statusCode),
         responseBodyType: this.callFactory(this._responseBodyType),
         responseJson: this.callFactory(this._responseJson),
         responseText: this.callFactory(this._responseText),
-        delay: this.callFactory(this._delay),
-        description: this.callFactory(this._description),
 
         ...(this._createdAt && {
           createdAt: this.callFactory(this._createdAt),
