3641da085de1a1698fddbf75ec9b7835af6e0199
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-03-08 11:20:16 -0300

    feat: add integration tests for FindEndpointUseCase to verify endpoint retrieval and error handling

---
+++ src/core/app/endpoint/find-endpoint/__tests__/find-endpoint.use-case.int-spec.ts
@@ -0,0 +1,57 @@
+import { IEndpointRepository } from "@domain/endpoint/endpoint.repository";
+import { FindEndpointUseCase } from "../find-endpoint.use-case";
+import { EndpointFactory } from "@domain/endpoint/endpoint.entity";
+import { Uuid } from "@domain/shared/value-objects/uuid.vo";
+import { NotFoundError } from "@domain/shared/errors/not-found.error";
+import { setupTypeOrm } from "@infra/shared/testing/helpers";
+import { EndpointModel } from "@infra/endpoint/db/typeorm/endpoint-typeorm.model";
+import { EndpointTypeOrmRepository } from "@infra/endpoint/db/typeorm/endpoint-typeorm.repository";
+
+describe("Find Endpoint Use Case - Integration Tests", () => {
+  let useCase: FindEndpointUseCase;
+  let repository: IEndpointRepository;
+
+  const { dataSource } = setupTypeOrm({
+    entities: [EndpointModel],
+  });
+
+  beforeEach(() => {
+    repository = new EndpointTypeOrmRepository(dataSource);
+    useCase = new FindEndpointUseCase(repository);
+  });
+
+  describe("execute()", () => {
+    it("should find an endpoint", async () => {
+      const endpoint = EndpointFactory.fake()
+        .oneEndpoint()
+        .withStatusCode(204)
+        .build();
+
+      await repository.insert(endpoint);
+
+      const foundEndpoint = await useCase.execute({
+        id: endpoint.entity_id.id,
+      });
+
+      expect(foundEndpoint).toEqual({
+        id: endpoint.entity_id.id,
+        title: endpoint.title,
+        method: endpoint.method,
+        statusCode: endpoint.statusCode,
+        description: endpoint.description,
+        delay: endpoint.delay,
+        createdAt: endpoint.createdAt,
+      });
+    });
+
+    it("should throw an error if endpoint not found", async () => {
+      const id = new Uuid();
+
+      await expect(
+        useCase.execute({
+          id: id.id,
+        }),
+      ).rejects.toThrow(NotFoundError);
+    });
+  });
+});
