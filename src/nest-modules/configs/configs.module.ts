6e8e158fabc4bf25177bba44a47651876d86ed71
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-03-22 14:59:07 -0300

    feat(config): add console log for environment variables in ConfigsModule

--- src/nest-modules/configs/configs.module.ts
+++ src/nest-modules/configs/configs.module.ts
@@ -1,3 +1,10 @@
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DATABASE_URL: process.env.DATABASE_URL,
+});
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import {
